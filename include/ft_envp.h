8dec4bccf665f3afb7fa58e171dc59fbe722fe41
Oscar Lo <oscarlo@Oscars-iMac.local>
2021-11-19 12:45:15 +0100

    envp fix

--- include/ft_envp.h
+++ include/ft_envp.h
@@ -6,7 +6,7 @@
 /*   By: olozano- <olozano-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 16:08:08 by user42            #+#    #+#             */
-/*   Updated: 2021/11/19 10:05:40 by olozano-         ###   ########.fr       */
+/*   Updated: 2021/11/19 12:00:51 by olozano-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 char	**ft_envpdup(char **envp);
 void	ft_getenv_var(char **separate, char **envp, int s_quote, int d_quote);
 void	ft_last_cmd(int wstatus, char **envp);
+void	ft_forget_env(char	**env_cpy);
 
 #endif
